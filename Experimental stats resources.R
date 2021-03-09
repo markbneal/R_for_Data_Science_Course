f0344249954d7b546222930c69dc414feee927c5
markbneal <mark.neal@dairynz.co.nz>
2021-03-09 16:45:01 +1300

    added experimental stats resources

---
+++ Experimental stats resources.R
@@ -0,0 +1,25 @@
+# experimental stats resources
+
+# Stat for Biology (Onofri)
+# https://www.statforbiology.com/_statbook/index.html
+# And her blog:
+# eg https://www.statforbiology.com/2020/stat_lte_modelbuilding/  
+
+
+# CRAN task view
+# https://cran.r-project.org/web/views/ExperimentalDesign.html
+
+#install.packages("agridat")
+library(agridat)
+?agridat
+
+#install.packages("agricolae")
+library(agricolae)
+# vignette
+# https://cran.r-project.org/web/packages/agricolae/vignettes/tutorial.pdf
+
+# R companion to Experimental Design (https://www.springer.com/gp/book/9783319645827)
+# https://cran.r-project.org/doc/contrib/Vikneswaran-ED_companion.pdf
+
+# Design and Analysis of Experiments with R
+# http://www.ru.ac.bd/stat/wp-content/uploads/sites/25/2019/03/502_07_00_Lawson_Design-and-Analysis-of-Experiments-with-R-2017.pdf
---
+++ workshop 3 test.R
@@ -0,0 +1,5 @@
+#Workshop 3 test
+
+library(tidyverse)
+
+library(modelr)
